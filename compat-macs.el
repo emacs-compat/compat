3f755dbbba1a51c86044c41e40ae827c8f77496a
Philip Kaludercic <philipk@posteo.net>
2022-02-26 14:23:41 +0100

    Move compat-def check into compat body
    As it is possible for multiple compat definitions to be given for the
    same function, the important thing is not to prevent this, but to
    ensure that the conditions prevent both to be applied.

--- compat-macs.el
+++ compat-macs.el
@@ -87,6 +87,9 @@ TYPE is used to set the symbol property `compat-type' for NAME."
          (realname (or (plist-get attr :realname)
                        (intern (format "compat--%S" name))))
          (body `(progn
+                  (when (get ',name 'compat-def)
+                    (error "Duplicate compatibility definition: %s" ',name))
+                  (put ',name 'compat-def ',realname)
                   ,(unless (plist-get attr :no-highlight)
                      `(font-lock-add-keywords
                        'emacs-lisp-mode
@@ -99,9 +102,6 @@ TYPE is used to set the symbol property `compat-type' for NAME."
        (put ',realname 'compat-type ',type)
        (put ',realname 'compat-version ,version)
        (put ',realname 'compat-doc ,(plist-get attr :note))
-       (when (get ',name 'compat-def)
-         (error "Duplicate compatibility definition: %s" ',name))
-       (put ',name 'compat-def ',realname)
        ,(funcall def-fn realname version)
        (,@(cond
            ((or (and min-version
