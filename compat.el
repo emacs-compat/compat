21bc2af911bbab44f5abc27f57920af0dae528a3
Philip Kaludercic <philipk@posteo.net>
2022-04-05 12:08:35 +0200

    Update URL header

--- compat.el
+++ compat.el
@@ -5,7 +5,7 @@
 ;; Author: Philip Kaludercic <philipk@posteo.net>
 ;; Maintainer: Compat Development <~pkal/compat-devel@lists.sr.ht>
 ;; Version: 28.1.0.0
-;; URL: https://git.sr.ht/~pkal/compat/
+;; URL: https://sr.ht/~pkal/compat
 ;; Package-Requires: ((emacs "24.3") (nadvice "0.3"))
 ;; Keywords: lisp
 
