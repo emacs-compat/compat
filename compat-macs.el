999ef2a730fb2d2a633c7a86c4e012fadc1b66cc
Philip Kaludercic <philipk@posteo.net>
2022-08-30 15:36:07 +0200

    Raise an error if the realname would be the same as the name

--- compat-macs.el
+++ compat-macs.el
@@ -119,6 +119,8 @@ DEF-FN, INSTALL-FN, CHECK-FN, ATTR and TYPE."
                   '(compat--ignore))
                  (`(when (and ,(if cond cond t)
                               ,(funcall check-fn)))))))
+    (when (eq name realname)
+      (error "%S: Name is equal to realname" name))
     (cond
      ((and (plist-get attr :prefix) (memq type '(func macro))
            (string-match "\\`compat-\\(.+\\)\\'" (symbol-name name))
