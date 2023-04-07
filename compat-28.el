ded87f826e2320358293e7cc417f3e660359cd90
Philip Kaludercic <philipk@posteo.net>
2023-04-07 12:14:13 +0200

    make-separator-line: Emulate 'separator-line' for graphical displays

--- compat-28.el
+++ compat-28.el
@@ -221,7 +221,9 @@ and BLUE, is normalized to have its value in [0,65535]."
 (compat-defun make-separator-line (&optional length) ;; <compat-tests:make-separator-line>
   "Make a string appropriate for usage as a visual separator line.
 If LENGTH is nil, use the window width."
-    (concat (make-string (or length (1- (window-width))) ?-) "\n"))
+  (if (display-graphic-p)
+      (propertize "\n" 'face '(:extend t :height 0.1 :inverse-video t))
+    (concat (make-string (or length (1- (window-width))) ?-) "\n")))
 
 ;;;; Defined in subr.el
 
