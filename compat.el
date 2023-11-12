6bed57b83afce3a352d5862b8444fd61ec9640e0
Daniel Mendler <mail@daniel-mendler.de>
2023-11-12 19:58:27 +0100

    The maintainer field accepts only a single mail address

--- compat.el
+++ compat.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2021-2023 Free Software Foundation, Inc.
 
 ;; Author: Philip Kaludercic <philipk@posteo.net>, Daniel Mendler <mail@daniel-mendler.de>
-;; Maintainer: Daniel Mendler <mail@daniel-mendler.de>, Compat Development <~pkal/compat-devel@lists.sr.ht>
+;; Maintainer: Compat Development <~pkal/compat-devel@lists.sr.ht>
 ;; Version: 29.1.4.3
 ;; URL: https://github.com/emacs-compat/compat
 ;; Package-Requires: ((emacs "24.4") (seq "2.23"))
