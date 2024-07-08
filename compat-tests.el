09dce8a193c5a70277512263782b82fa1cba84c0
Daniel Mendler <mail@daniel-mendler.de>
2024-07-08 20:20:06 +0200

    Version 30.0.0.0

--- compat-tests.el
+++ compat-tests.el
@@ -92,7 +92,7 @@
 (defconst compat-tests--version (package-get-version))
 (ert-deftest compat-package-get-version ()
   (should (stringp compat-tests--version))
-  (should-equal 29 (car (version-to-list compat-tests--version))))
+  (should-equal 30 (car (version-to-list compat-tests--version))))
 
 (ert-deftest compat-buffer-match-p ()
   (let ((b "*compat-test-buffer*")
